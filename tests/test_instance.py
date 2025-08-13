5a43e3d0c80f375c608ac8d7133a22ae28458def
duytanisme <duytanisme02@gmail.com>
2025-08-13 23:58:20 +0700

    initial commit

---
+++ tests/test_instance.py
@@ -0,0 +1,48 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+
+from bmailer import AsyncMailer
+from bmailer.models import EmailPayload
+
+load_dotenv()
+
+SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
+SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
+SMTP_USERNAME = os.getenv("SMTP_USERNAME", "")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "")
+
+RECIPIENT = os.getenv("RECIPIENT", "")
+BODY = "This is a test email."
+SUBJECT = "Test Email"
+
+async_mailer = AsyncMailer(
+    host=SMTP_HOST,
+    port=SMTP_PORT,
+    username=SMTP_USERNAME,
+    password=SMTP_PASSWORD,
+)
+
+email = EmailPayload(subject=SUBJECT, recipient=RECIPIENT, body=BODY)
+
+
+def test_async_mailer_initialization():
+    assert async_mailer is not None
+    assert hasattr(async_mailer, "send_email")
+    assert hasattr(async_mailer, "send_bulk")
+    assert async_mailer.host == SMTP_HOST
+    assert async_mailer.port == SMTP_PORT
+    assert async_mailer.username == SMTP_USERNAME or SMTP_USERNAME == ""
+    assert async_mailer.from_email == SMTP_USERNAME or SMTP_USERNAME == ""
+    assert str(async_mailer) == "AsyncMailer"
+
+
+@pytest.mark.asyncio
+async def test_async_mailer_integration_concurr():
+    await async_mailer.send_bulk(emails=[email] * 5, concurrency=5)
+
+
+@pytest.mark.asyncio
+async def test_async_mailer_integration_sync():
+    await async_mailer.send_bulk_sync(emails=[email])
